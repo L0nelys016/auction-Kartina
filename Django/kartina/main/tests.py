e2b7703836ae86469dde25f2201e043bfa996f30
unknown <@ngknn.local>
2025-06-03 10:25:41 +0300

    Complete Unit-test

--- Django/kartina/main/tests.py
+++ Django/kartina/main/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from .models import (
+    modern_picture,
+    classic_picture,
+    abstract_picture,
+    portret_picture,
+    favorites_picture
+)
 
-# Create your tests here.
+class PictureModelsTestCase(TestCase):
+
+    def setUp(self):
+        # Создаем базовые объекты для тестирования
+        self.modern = modern_picture.objects.create(
+            title='Современная картина',
+            price=1000,
+            picture='path/to/modern.jpg'
+        )
+        self.classic = classic_picture.objects.create(
+            title='Классическая картина',
+            price=2000,
+            picture='path/to/classic.jpg'
+        )
+        self.abstract = abstract_picture.objects.create(
+            title='Абстрактная картина',
+            price=1500,
+            picture='path/to/abstract.jpg'
+        )
+        
+        
+
+
+    def test_modern_picture_str(self):
+        self.assertEqual(str(self.modern), 'Современная картина')
+
+    def test_classic_picture_str(self):
+        self.assertEqual(str(self.classic), 'Классическая картина')
+
+    def test_abstract_picture_str(self):
+        self.assertEqual(str(self.abstract), 'Абстрактная картина')
+
+   
+
+    
