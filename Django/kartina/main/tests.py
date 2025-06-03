59d162f32a202b39fc353501ea2d78353a6cbf37
Волков А. А. <@ngknn.local>
2025-06-03 10:35:01 +0300

    Complete Unit-test by ArtemVolkov

--- Django/kartina/main/tests.py
+++ Django/kartina/main/tests.py
@@ -28,6 +28,21 @@ class PictureModelsTestCase(TestCase):
         )
         
         
+        self.portret = portret_picture.objects.create(
+                title='Портрет',
+                price=1200,
+                picture='path/to/portret.jpg'
+            )
+
+
+        self.favorites = favorites_picture.objects.create(
+                title='Избранное',
+                price=3000,
+                picture='favorites/fav1.jpg',
+                original_id=self.modern.id,
+                picture_type='modern'
+            )
+
 
 
     def test_modern_picture_str(self):
@@ -38,7 +53,19 @@ class PictureModelsTestCase(TestCase):
 
     def test_abstract_picture_str(self):
         self.assertEqual(str(self.abstract), 'Абстрактная картина')
-
    
+    def test_favorites_picture_unique_together(self):
+        # Проверка ограничения уникальности
+        with self.assertRaises(Exception):
+            favorites_picture.objects.create(
+                title='Дубликат',
+                price=500,
+                picture='favorites/fav2.jpg',
+                original_id=self.modern.id,
+                picture_type='modern'
+            )
+
+    def test_portret_picture_str(self):
+        self.assertEqual(str(self.portret), 'Портрет')
 
     
